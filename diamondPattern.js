a2b2e481b5e8352f681dab259256207fe83462a3
pravu-Ranjan <pravu_ps7@outlook.com>
2022-09-13 20:38:49 +0530

    diamond kata pattern solved

---
+++ diamondPattern.js
@@ -0,0 +1,40 @@
+function diamondKata() {
+  let originalString = 'ABCDE'
+  for (let i = 0; i < originalString.length; i++) {
+    let tempString = ''
+    for (let j = originalString.length - 1; j > 0; j--) {
+      if (originalString[i] === originalString[j]) {
+        tempString += originalString[j]
+      } else {
+        tempString += ' '
+      }
+    }
+    for (let j = 0; j < originalString.length; j++) {
+      if (originalString[i] === originalString[j]) {
+        tempString += originalString[j]
+      } else {
+        tempString += ' '
+      }
+    }
+    console.log(tempString)
+  }
+  for (let i = originalString.length - 2; i >= 0; i--) {
+    let tempString = ''
+    for (let j = originalString.length - 1; j >= 0; j--) {
+      if (originalString[i] === originalString[j]) {
+        tempString += originalString[j]
+      } else {
+        tempString += ' '
+      }
+    }
+    for (let j = 1; j < originalString.length; j++) {
+      if (originalString[i] === originalString[j]) {
+        tempString += originalString[j]
+      } else {
+        tempString += ' '
+      }
+    }
+    console.log(tempString)
+  }
+}
+diamondKata()
